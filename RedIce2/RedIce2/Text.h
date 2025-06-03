2dec6a954b05fa7d40897b9eafc340dabd4e9207
Anna Evddokimova <ivanevdokimov802@gmail.com>
2025-06-03 20:28:50 +0300

    Полный текст Level_1

--- RedIce2/RedIce2/Text.h
+++ RedIce2/RedIce2/Text.h
@@ -12,12 +12,25 @@ public:
 	{
 		static void HelloPablo();
 		static void Strategy1();
+		// текст для атаки в пустыни
 		static void PeopleDesert();
 		static void StrategyDesert();
 		static void DesertAttak();
 		static void DesertDefense();
 		static void Death1();
 		static void Survive();
+		static void Win1();
+		// текст для логова врага
+		static void PeopleLair();
+		static void AttakLair();
+		static void Win2();
+		// текст пригласить в свое логово
+		static void Nothing();
+		// текст переговоры
+		static void Negotations();
+		static void WinNegotations1();
+		static void WinNegotations2();
+		static void LoseNegotations();
 	};
 	struct Level2
 	{
