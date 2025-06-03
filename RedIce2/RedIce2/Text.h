ffb49303956cc23e0aaae4f0ee591bec661159d8
BiktagirovaDiana <biktagirovad@yandex.ru>
2025-06-03 14:33:00 +0300

    Добавление класса Text

--- RedIce2/RedIce2/Text.h
+++ RedIce2/RedIce2/Text.h
@@ -1,5 +1,30 @@
 #pragma once
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
 class Text
 {
+	struct Level_1
+	{
+		void HelloPablo();
+	};
+	struct Level_2
+	{
+
+	};
+	struct Level_3
+	{
+
+	};
+	struct Level_4
+	{
+
+	};
+	struct Level_5
+	{
+
+	};
 };
 
