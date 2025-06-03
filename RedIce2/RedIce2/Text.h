5e1c5d4a2a45b929eeefc7c6a97e83ad8242324d
BiktagirovaDiana <biktagirovad@yandex.ru>
2025-06-03 18:45:09 +0300

    Добавление текста

--- RedIce2/RedIce2/Text.h
+++ RedIce2/RedIce2/Text.h
@@ -11,6 +11,13 @@ public:
 	struct Level1
 	{
 		static void HelloPablo();
+		static void Strategy1();
+		static void PeopleDesert();
+		static void StrategyDesert();
+		static void DesertAttak();
+		static void DesertDefense();
+		static void Death1();
+		static void Survive();
 	};
 	struct Level2
 	{
