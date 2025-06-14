d18ae9cb371ce9213d4b4b1b6d5953971d456939
BiktagirovaDiana <biktagirovad@yandex.ru>
2025-06-14 18:42:29 +0300

    .

--- RedIce2/RedIce2/Level_4.cpp
+++ RedIce2/RedIce2/Level_4.cpp
@@ -1,6 +1,87 @@
 #include "Level_4.h"
+#include "GameManager.h"
+#include "Shop.h"
 
-void Level_4::StartLevel() 
+using std::cout;
+using std::cin;
+using std::endl;
+
+void Level_4::StartLevel()
 {
+	int choice = 0;
+	Text::Level4::HelloPablo4();
+	cin >> choice;
+
+	switch (choice)
+	{
+	case 1:
+		PreparationForSearch();
+		break;
+	case 2:
+		Text::Level4::BuyOff();
+		GameManager::Lose();
+		break;
+	case 3:
+		Raid();
+		break;
+
+	}
+}
+void Level_4::PreparationForSearch()
+{
+	int choice = 0;
+	Text::Level4::Peaceful();
+	cin >> choice;
+	switch (choice)
+	{
+	case 1:
+		PreparationForSearch2();
+		break;
+	case 2:
+		GameManager::Lose();
+		break;
+	}
+}
+void Level_4::PreparationForSearch2()
+{
+	int choice = 0;
+	Text::Level4::Peaceful2();
+	cin >> choice;
+	Text::Level4::WinPolice();
+}
+
+void Level_4::Raid()
+{
+	int choice = 0;
+	Text::Level4::AttackPolice();
+	cin >> choice;
+	switch (choice)
+	{
+	case 1:
+		Text::Level4::Warning();
+
+		//Shop::BuyOrNor();
+		break;
+	case 2:
+		WaitRaid();
+		break;
+	}
+}
+
+void Level_4::WaitRaid()
+{
+	int choice = 0;
+	Text::Level4::Search();
+	cin >> choice;
+	switch (choice)
+	{
+	case 1:
+		PreparationForSearch();
+		break;
+	case 2:
+		Text::Level4::Fight();
+		GameManager::Lose();
+		break;
+	}
 
 }
--- RedIce2/RedIce2/Level_4.h
+++ RedIce2/RedIce2/Level_4.h
@@ -1,9 +1,21 @@
 #pragma once
 #include "Text.h"
+//#include "GameManager.h"
+#include <iostream>
+
+class GameManager;
+class Shop;
 
 class Level_4
 {
 public:
 	void StartLevel();
+	void PreparationForSearch();
+	void PreparationForSearch2();
+	void Raid();
+	void WaitRaid();
+private:
+	Text Text;
+	//GameManager GameManager;
 };
 
