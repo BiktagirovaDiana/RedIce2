0f05b238306998d47c9a6fbc20038c18824715c8
AnnaEvdokimova <ivanevdokimov802@gmail.com>
2025-06-15 17:26:27 +0300

    Уровень 2

--- RedIce2/RedIce2/Level_2.cpp
+++ RedIce2/RedIce2/Level_2.cpp
@@ -1,6 +1,287 @@
 #include "Level_2.h"
 
-void Level_2::StartLevel() 
+void Level_2::StartLevel()
 {
+    int choice = 0;
+    PlayerController::InfoPlayer();
+    Text::Level2::HelloPablo2();
+    cin >> choice;
 
+    switch (choice)
+    {
+    case 1:
+        Attack();
+        break;
+    case 2:
+        Text::Level2::PayOff();
+        Attack();
+        break;
+    default:
+        break;
+    }
+}
+
+void Level_2::Attack()
+{
+    shop.BuyOrNor();
+    PlayerController::InfoPlayer();
+    int choice = 0;
+    Text::Level2::Strategy2();
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+        Trap();
+        break;
+    case 2:
+        PeopleAttack();
+        break;
+    case 3:
+        Text::Level2::PayOff();
+        PeopleAttack();
+        break;
+    default:
+        break;
+    }
+}
+
+void Level_2::Trap()
+{
+    int choice = 0;
+    Text::Level2::ChooseTrap();
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+        Text::Level2::Trap1();
+        PlayerController::setInfluence(PlayerController::getInfluence() - 15);
+        PlayerController::InfoPlayer();
+        PeopleAttack();
+        break;
+    case 2:
+        Text::Level2::Trap2();
+        enemy.takeDamage(20);
+        PeopleAttack();
+        break;
+    default:
+        break;
+    }
+}
+
+void Level_2::PeopleAttack()
+{
+    int choice = 0;
+    Text::Level2::PeopleAttack();
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+        if (PlayerController::getMoney() >= 800)
+        {
+            Killers = 4;
+            PlayerController::setMoney(PlayerController::getMoney() - 800);
+            PlayerController::InfoPlayer();
+            AttackPlace();
+        }
+        else
+        {
+            GameManager::Lose();
+        }
+        break;
+    case 2:
+        if (PlayerController::getMoney() >= 1200)
+        {
+            Killers = 6;
+            PlayerController::setMoney(PlayerController::getMoney() - 1200);
+            PlayerController::InfoPlayer();
+            AttackPlace();
+        }
+        else
+        {
+            GameManager::Lose();
+        }
+        break;
+    case 3:
+        if (PlayerController::getMoney() >= 1600)
+        {
+            Killers = 8;
+            PlayerController::setMoney(PlayerController::getMoney() - 1600);
+            PlayerController::InfoPlayer();
+            AttackPlace();
+        }
+        else
+        {
+            GameManager::Lose();
+        }
+        break;
+    default:
+        break;
+    }
+}
+
+void Level_2::AttackPlace()
+{
+    int choice = 0;
+    Text::Level2::AttackPlace();
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+        Text::Level2::Restaurant();
+        StrategyAttack();
+        break;
+    case 2:
+        Text::Level2::Desert();
+        StrategyAttack();
+        break;
+    case 3:
+        Text::Level2::Station();
+        PlayerController::setInfluence(PlayerController::getInfluence() - 20);
+        PlayerController::InfoPlayer();
+        StrategyAttack();
+        break;
+    default:
+        break;
+    }
+}
+
+void Level_2::StrategyAttack()
+{
+    int choice = 0;
+    Text::Level2::StrategyAttack();
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+        Text::Level2::AttackFirst();
+        if (PlayerController::getGun() >= 2)
+        {
+            PlayerController::setGun(PlayerController::getGun() - 2);
+            enemy.takeDamage(20);
+            PlayerController::InfoPlayer();
+            ContinueStrat();
+        }
+        else
+        {
+            GameManager::Lose();
+        }
+        break;
+    case 2:
+        PlayerController::setHP(PlayerController::getHP() - 15);
+        if (PlayerController::getGun() >= 2)
+        {
+            PlayerController::setGun(PlayerController::getGun() - 2);
+            enemy.takeDamage(10);
+            PlayerController::InfoPlayer();
+            ContinueStrat();
+        }
+        else
+        {
+            GameManager::Lose();
+        }
+        break;
+    default:
+        break;
+    }
+}
+
+void Level_2::ContinueStrat()
+{
+    int choice = 0;
+    Text::Level2::ContinueStrat();
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+        if (PlayerController::getGrenade() >= 1)
+        {
+            PlayerController::setGrenade(PlayerController::getGrenade() - 1);
+            enemy.takeDamage(20);
+            PlayerController::InfoPlayer();
+            Text::Level2::ifGrenade();
+            FinalPart();
+        }
+        else
+        {
+            GameManager::Lose();
+        }
+        break;
+    case 2:
+        if (PlayerController::getGun() >= 3)
+        {
+            PlayerController::setGun(PlayerController::getGun() - 3);
+            enemy.takeDamage(20);
+            PlayerController::InfoPlayer();
+            Text::Level2::ifGuns();
+            FinalPart();
+        }
+        else
+        {
+            GameManager::Lose();
+        }
+        break;
+    default:
+        break;
+    }
+}
+
+void Level_2::FinalPart()
+{
+    if (enemy.getHP() <= 0)
+    {
+        Text::Level2::WinL2Short();
+        PlayerController::setMoney(PlayerController::getMoney() + 2000);
+        PlayerController::setInfluence(PlayerController::getInfluence() + 20);
+        PlayerController::setHP(PlayerController::getHP() + 20);
+        PlayerController::InfoPlayer();
+    }
+    else
+    {
+        int choice = 0;
+        Text::Level2::FinalPart();
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            if (PlayerController::getGun() >= 2)
+            {
+                PlayerController::setGun(PlayerController::getGun() - 2);
+                enemy.takeDamage(20);
+                Text::Level2::WinL2();
+                PlayerController::setMoney(PlayerController::getMoney() + 2000);
+                PlayerController::setInfluence(PlayerController::getInfluence() + 10);
+                PlayerController::setHP(PlayerController::getHP() + 20);
+                PlayerController::InfoPlayer();
+            }
+            else
+            {
+                GameManager::Lose();
+            }
+            break;
+        case 2:
+            if (PlayerController::getGrenade() >= 1)
+            {
+                PlayerController::setGrenade(PlayerController::getGrenade() - 1);
+                enemy.takeDamage(20);
+                Text::Level2::WinL2();
+                PlayerController::setMoney(PlayerController::getMoney() + 2000);
+                PlayerController::setHP(PlayerController::getHP() + 20);
+                PlayerController::InfoPlayer();
+            }
+            else
+            {
+                GameManager::Lose();
+            }
+            break;
+        default:
+            break;
+        }
+    }
 }
--- RedIce2/RedIce2/Level_2.h
+++ RedIce2/RedIce2/Level_2.h
@@ -1,9 +1,30 @@
-#pragma once
+﻿#pragma once
 #include "Text.h"
+#include "PlayerController.h"
+#include "GameManager.h"
+#include "Shop.h"
+#include "Enemies.h"
 
 class Level_2
 {
-public:
-	void StartLevel();
-};
+private:
+    int Killers = 0;
+    Shop shop;
+    Enemies enemy;
 
+public:
+    Level_2() : enemy(60) {}
+
+    void StartLevel();
+    // атака
+    void Attack();
+    // ловушка
+    void Trap();
+    // перестрелка
+    void PeopleAttack();
+    void AttackPlace();
+    void StrategyAttack();
+    // стратегия атаки
+    void ContinueStrat();
+    void FinalPart();
+};
