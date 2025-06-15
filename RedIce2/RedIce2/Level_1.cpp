51e1045d9d2c92000a730a6441633d5e4a77547f
AnnaEvdokimova <ivanevdokimov802@gmail.com>
2025-06-15 17:54:56 +0300

    Усовершенствованный уровень 1

--- RedIce2/RedIce2/Level_1.cpp
+++ RedIce2/RedIce2/Level_1.cpp
@@ -2,219 +2,315 @@
 
 void Level_1::StartLevel()
 {
-	int choice = 0;
-	PlayerController::InfoPlayer();
-	Text::Level1::HelloPablo1();
-	cin >> choice;
+    int choice = 0;
+    PlayerController::InfoPlayer();
+    Text::Level1::HelloPablo1();
+    cin >> choice;
 
-	switch (choice)
-	{
-	case 1:
-		DestroyEnemy();
-		break;
-	case 2:
-		Negotiations();
-		break;
-	}
+    switch (choice)
+    {
+    case 1:
+        DestroyEnemy();
+        break;
+    case 2:
+        Negotiations();
+        break;
+    default:
+        GameManager::Lose();
+        break;
+    }
 }
 
-// атака или откуп
 void Level_1::DestroyEnemy()
 {
-	int choice = 0;
-	Text::Level1::Strategy1();
-	cin >> choice;
+    int choice = 0;
+    Text::Level1::Strategy1();
+    cin >> choice;
 
-	switch (choice)
-	{
-	case 1:
-		shop.BuyOrNor();
-		PlayerController::InfoPlayer();
-		PeopleDesert();
-		break;
-	case 2:
-		shop.BuyOrNor();
-		PlayerController::InfoPlayer();
-		EnemysLair();
-		break;
-	case 3:
-		Text::Level1::Nothing();
-		DestroyEnemy();
-		break;
-	default:
-		break;
-	}
+    switch (choice)
+    {
+    case 1:
+        shop.BuyOrNor();
+        PeopleDesert();
+        break;
+    case 2:
+        shop.BuyOrNor();
+        EnemysLair();
+        break;
+    case 3:
+        Text::Level1::Nothing();
+        DestroyEnemy();
+        break;
+    default:
+        GameManager::Lose();
+        break;
+    }
 }
 
 void Level_1::Negotiations()
 {
-	int choice = 0;
-	Text::Level1::Negotations();
-	cin >> choice;
+    int choice = 0;
+    Text::Level1::Negotations();
+    cin >> choice;
 
-	switch (choice)
-	{
-	case 1:
-		Text::Level1::WinNegotations1();
-		PlayerController::setInfluence(PlayerController::getInfluence() - 15);
-		PlayerController::setMoney(PlayerController::getMoney() - 2000);
-		break;
-	case 2:
-		Text::Level1::WinNegotations2();
-		PlayerController::setInfluence(PlayerController::getInfluence() - 10);
-		PlayerController::setMoney(PlayerController::getMoney() - 1000);
-		break;
-	case 3:
-		Text::Level1::LoseNegotations();
-		GameManager::Lose();
-		break;
-	default:
-		break;
-	}
-
+    switch (choice)
+    {
+    case 1:
+        if (PlayerController::getMoney() >= 2000)
+        {
+            Text::Level1::WinNegotations1();
+            PlayerController::setInfluence(PlayerController::getInfluence() - 15);
+            PlayerController::setMoney(PlayerController::getMoney() - 2000); 
+        }
+        else
+        {
+            GameManager::Lose();
+        }
+        break;
+    case 2:
+        if (PlayerController::getMoney() >= 1000)
+        {
+            Text::Level1::WinNegotations2();
+            PlayerController::setInfluence(PlayerController::getInfluence() - 10);
+            PlayerController::setMoney(PlayerController::getMoney() - 1000); 
+        }
+        else
+        {
+            GameManager::Lose();
+        }
+        break;
+    case 3:
+        Text::Level1::LoseNegotations();
+        GameManager::Lose();
+        break;
+    default:
+        GameManager::Lose();
+        break;
+    }
+    PlayerController::InfoPlayer(); 
 }
 
-// в пустыне
 void Level_1::PeopleDesert()
 {
-	int choice = 0;
-	Text::Level1::PeopleDesert();
-	cin >> choice;
+    int choice = 0;
+    Text::Level1::PeopleDesert();
+    cin >> choice;
 
-	switch (choice)
-	{
-	case 1:
-		Killers = 2;
-		PlayerController::setMoney(PlayerController::getMoney() - 700);
-		PlayerController::InfoPlayer();
-		break;
-	case 2:
-		Killers = 2;
-		PlayerController::setMoney(PlayerController::getMoney() - 750);
-		PlayerController::InfoPlayer();
-		break;
-	case 3:
-		Killers = 4;
-		PlayerController::setMoney(PlayerController::getMoney() - 1100);
-		PlayerController::InfoPlayer();
-		break;
-	case 4:
-		Killers = 4;
-		PlayerController::setMoney(PlayerController::getMoney() - 1500);
-		PlayerController::InfoPlayer();
-		break;
-	default:
-		break;
-	}
+    switch (choice)
+    {
+    case 1:
+        if (PlayerController::getMoney() >= 700)
+        {
+            Killers = 2;
+            PlayerController::setMoney(PlayerController::getMoney() - 700);
+            StrategyDesert();
+        }
+        else
+        {
+            GameManager::Lose();
+        }
+        break;
+    case 2:
+        if (PlayerController::getMoney() >= 750)
+        {
+            Killers = 2;
+            PlayerController::setMoney(PlayerController::getMoney() - 750);
+            StrategyDesert();
+        }
+        else
+        {
+            GameManager::Lose();
+        }
+        break;
+    case 3:
+        if (PlayerController::getMoney() >= 1100)
+        {
+            Killers = 4;
+            PlayerController::setMoney(PlayerController::getMoney() - 1100);
+            StrategyDesert();
+        }
+        else
+        {
+            GameManager::Lose();
+        }
+        break;
+    case 4:
+        if (PlayerController::getMoney() >= 1500)
+        {
+            Killers = 4;
+            PlayerController::setMoney(PlayerController::getMoney() - 1500);
+            StrategyDesert();
+        }
+        else
+        {
+            GameManager::Lose();
+        }
+        break;
+    default:
+        GameManager::Lose();
+        break;
+    }
 }
 
 void Level_1::StrategyDesert()
 {
-	int choice = 0;
-	Text::Level1::StrategyDesert();
-	cin >> choice;
+    int choice = 0;
+    Text::Level1::StrategyDesert();
+    cin >> choice;
 
-	switch (choice)
-	{
-	case 1:
-		Text::Level1::DesertAttak();
-		enemy.takeDamage(20);
-		PlayerController::setGun(PlayerController::getGun() - 3);
-		PlayerController::InfoPlayer();
-		break;
-	case 2:
-		Text::Level1::DesertDefense();
-		enemy.takeDamage(20);
-		PlayerController::setGun(PlayerController::getGun() - 4);
-		PlayerController::setHP(PlayerController::getHP() + 20);
-		PlayerController::InfoPlayer();
-		break;
-	default:
-		break;
-	}
+    switch (choice)
+    {
+    case 1:
+        if (PlayerController::getGun() >= 3)
+        {
+            Text::Level1::DesertAttak();
+            enemy.takeDamage(20);
+            PlayerController::setGun(PlayerController::getGun() - 3);
+            LoseOrWin();
+        }
+        else
+        {
+            GameManager::Lose();
+        }
+        break;
+    case 2:
+        if (PlayerController::getGun() >= 4) 
+        {
+            Text::Level1::DesertDefense();
+            enemy.takeDamage(20);
+            PlayerController::setGun(PlayerController::getGun() - 4);
+            PlayerController::setHP(PlayerController::getHP() - 20); 
+            LoseOrWin();
+        }
+        else
+        {
+            GameManager::Lose();
+        }
+        break;
+    default:
+        GameManager::Lose();
+        break;
+    }
+    PlayerController::InfoPlayer();
 }
 
 void Level_1::LoseOrWin()
 {
-	if (Killers == 4)
-	{
-		Text::Level1::Survive();
-		Text::Level1::Win1();
-		enemy.takeDamage(20);
-		PlayerController::setGun(PlayerController::getGun() - 2);
-		PlayerController::setMoney(PlayerController::getMoney() + 1000);
-		PlayerController::setInfluence(PlayerController::getInfluence() + 10);
-		PlayerController::setHP(PlayerController::getHP() + 20);
-		PlayerController::InfoPlayer();
-	}
-	else
-	{
-		Text::Level1::Death1();
-		GameManager::Lose();
-	}
+    if (Killers == 4)
+    {
+        if (PlayerController::getGun() >= 2) 
+        {
+            Text::Level1::Survive();
+            enemy.takeDamage(20);
+            PlayerController::setGun(PlayerController::getGun() - 2);
+            Text::Level1::Win1();
+            PlayerController::setMoney(PlayerController::getMoney() + 1000);
+            PlayerController::setInfluence(PlayerController::getInfluence() + 10);
+            PlayerController::setHP(PlayerController::getHP() + 20);
+        }
+        else
+        {
+            GameManager::Lose();
+        }
+    }
+    else
+    {
+        Text::Level1::Death1();
+        GameManager::Lose();
+    }
+    PlayerController::InfoPlayer();
 }
 
-// в логове врага
 void Level_1::EnemysLair()
 {
-	int choice = 0;
-	Text::Level1::PeopleLair();
-	cin >> choice;
+    int choice = 0;
+    Text::Level1::PeopleLair();
+    cin >> choice;
 
-	switch (choice)
-	{
-	case 1:
-		Killers = 2;
-		PlayerController::setMoney(PlayerController::getMoney() - 700);
-		PlayerController::InfoPlayer();
-		break;
-	case 2:
-		Killers = 3;
-		PlayerController::setMoney(PlayerController::getMoney() - 900);
-		PlayerController::InfoPlayer();
-		break;
-	case 3:
-		Killers = 4;
-		PlayerController::setMoney(PlayerController::getMoney() - 1200);
-		PlayerController::InfoPlayer();
-		break;
-	default:
-		break;
-	}
+    switch (choice)
+    {
+    case 1:
+        if (PlayerController::getMoney() >= 700)
+        {
+            Killers = 2;
+            PlayerController::setMoney(PlayerController::getMoney() - 700);
+            StrategyLair();
+        }
+        else
+        {
+            GameManager::Lose();
+        }
+        break;
+    case 2:
+        if (PlayerController::getMoney() >= 900)
+        {
+            Killers = 3;
+            PlayerController::setMoney(PlayerController::getMoney() - 900);
+            StrategyLair();
+        }
+        else
+        {
+            GameManager::Lose();
+        }
+        break;
+    case 3:
+        if (PlayerController::getMoney() >= 1200)
+        {
+            Killers = 4;
+            PlayerController::setMoney(PlayerController::getMoney() - 1200);
+            StrategyLair();
+        }
+        else
+        {
+            GameManager::Lose();
+        }
+        break;
+    default:
+        GameManager::Lose();
+        break;
+    }
 }
 
 void Level_1::StrategyLair()
 {
-	int choice = 0;
+    if (PlayerController::getGun() >= 2)
+    {
+        PlayerController::setGun(PlayerController::getGun() - 2);
+        PlayerController::setHP(PlayerController::getHP() - 15);
 
-	Text::Level1::AttakLair();
-	enemy.takeDamage(10);
-	PlayerController::setGun(PlayerController::getGun() - 2);
-	PlayerController::setHP(PlayerController::getHP() - 15);
+        int choice = 0;
+        Text::Level1::AttakLair();
+        enemy.takeDamage(10);
+        PlayerController::InfoPlayer();
+        cin >> choice;
 
-	cin >> choice;
-
-	switch (choice)
-	{
-	case 1:
-		shop.BuyOrNor();
-		PeopleDesert();
-		break;
-	case 2:
-		Text::Level1::Win2();
-		PlayerController::setInfluence(PlayerController::getInfluence() - 15);
-		if (PlayerController::setMoney(PlayerController::getMoney()) > 3000)
-		{
-			PlayerController::setMoney(PlayerController::getMoney() - 3000);
-			PlayerController::InfoPlayer();
-		}
-		else
-		{
-			GameManager::Lose();
-		}
-		break;
-	default:
-		break;
-	}
+        switch (choice)
+        {
+        case 1:
+            shop.BuyOrNor();
+            PeopleDesert();
+            break;
+        case 2:
+            if (PlayerController::getMoney() >= 3000) 
+            {
+                Text::Level1::Win2();
+                PlayerController::setInfluence(PlayerController::getInfluence() - 15);
+                PlayerController::setMoney(PlayerController::getMoney() - 3000);
+            }
+            else
+            {
+                GameManager::Lose();
+            }
+            break;
+        default:
+            GameManager::Lose();
+            break;
+        }
+    }
+    else
+    {
+        GameManager::Lose();
+    }
+    PlayerController::InfoPlayer();
 }
 
--- RedIce2/RedIce2/Level_1.h
+++ RedIce2/RedIce2/Level_1.h
@@ -5,32 +5,27 @@
 #include "Shop.h"
 #include "Enemies.h"
 
-using std::cout;
-using std::cin;
-using std::endl;
-
 class Level_1
 {
 private:
-	int Killers = 0;
-	Shop shop;
-	Enemies enemy;
+    int Killers = 0;
+    Shop shop;
+    Enemies enemy;
 
 public:
-	Level_1() : enemy(40) {}
+    Level_1() : enemy(40) {}
 
-	void StartLevel();
-	// стратегии
-	void DestroyEnemy();
-	void Negotiations();
-	// бой в пустыне
-	void PeopleDesert();
-	void StrategyDesert();
-	void LoseOrWin();
-	// бой в логове врага
-	void EnemysLair();
-	void StrategyLair();
-
+    void StartLevel();
+    // стратегии
+    void DestroyEnemy();
+    void Negotiations();
+    // бой в пустыне
+    void PeopleDesert();
+    void StrategyDesert();
+    void LoseOrWin();
+    // бой в логове врага
+    void EnemysLair();
+    void StrategyLair();
 };
 
 
